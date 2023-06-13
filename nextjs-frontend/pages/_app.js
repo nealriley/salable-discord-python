c87241aae21c0135f8c0309bf5b978ffa4f8463a
nealriley <neal.riley@gmail.com>
2023-06-13 22:50:11 +0000

    added example

--- nextjs-frontend/pages/_app.js
+++ nextjs-frontend/pages/_app.js
@@ -54,6 +54,14 @@ const App = ({ Component, pageProps }) => {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5"
         />
+        <script dangerouslySetInnerHTML={{__html: `
+            (function(I,n,f,o,b,i,p){
+            I[b]=I[b]||function(){(I[b].q=I[b].q||[]).push(arguments)};
+            I[b].t=1*new Date();i=n.createElement(f);i.async=1;i.src=o;
+            p=n.getElementsByTagName(f)[0];p.parentNode.insertBefore(i,p)})
+            (window,document,'script','https://livechat.infobip.com/widget.js','liveChat');
+
+            liveChat('init', '490b5be2-4b6e-4589-bd76-84a1c04a952d');`}}/>  
       </Head>
       <ClerkProvider {...pageProps}>
         <SalableProvider {...pageProps}>
